e7812bdb9a73a6ee7950d73d925ca4bf4a32584d
lh-25 <sprinklecake25@gmail.com>
2024-11-04 11:11:40 -0600

    setup complete

---
+++ src/components/PokemonList.jsx
@@ -0,0 +1,13 @@
+export default function PokemonList (props) {
+  return (
+    <>
+      <h2>Pokemon</h2>
+      <ul>
+        {props.pokemon.map((currentPokemon) => (
+          <li key={currentPokemon.name}>{currentPokemon.name}</li>
+        ))}
+      </ul>
+    </>
+  );
+};
+
